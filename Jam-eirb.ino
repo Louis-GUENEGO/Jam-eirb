b10af1900ee892afbb5880c08bae6bde4b039ca0
Louis GUENEGO <louis.guenego@gmail.com>
2021-11-17 10:58:18 +0100

    update

--- Jam-eirb.ino
+++ Jam-eirb.ino
@@ -8,52 +8,45 @@
 unsigned short int resultCAN = 0; // le résultat de la conversion A/N
 double mesure = 0; // Le résultat de la conversion en Volts
 
-unsigned int state;
+unsigned int time_ms;
 
 void setup() {
     analogReadResolution(RESOLUTION); // configuration du CAN
 
-    state = 0;
+    time_ms = 0;
     
-    tcConfigure(1000000); //configure the timer to run at <sampleRate>Hertz
-    tcStartCounter(); //starts the timer
+    tcConfigure();
+    tcStartCounter();
 }
+
 
 
 void loop() {
   
-  
+  //void
 
 }
 
-//Serial.print(resultCAN); // cette valeur n'est pas moyennée
-//Serial.print("\n");
 
 
 
-
-
-//this function gets called by the interrupt at <sampleRate>Hertz
-void TC5_Handler (void) {
-  //YOUR CODE HERE 
+void TC5_Handler (void) { // TC5 interrupt routine
   
   resultCAN = analogRead(PIN_ANALOGIQUE); // Conversion A/N
   
   if ( resultCAN > 1000 ){
-    if (state == 0) {
-        state = 1;
+    if (time_ms == 0) {
+        time_ms = 1;
     } else {
-      state++;
+      time_ms++;
     }
-  } else if ( state > 25 ) {
-      Serial.print(state); // cette valeur n'est pas moyennée
+  } else if ( time_ms > 25 ) {
+      Serial.print(time_ms);
       Serial.print(" ms\n");
-      state = 0;
+      time_ms = 0;
   }
 
-
-  // END OF YOUR CODE
-  TC5->COUNT16.INTFLAG.bit.MC0 = 1; //Writing a 1 to INTFLAG.bit.MC0 clears the interrupt so that it will run again
+  TC5->COUNT16.INTFLAG.bit.MC0 = 1; // clear interrup flag
 }
 
 
@@ -61,35 +54,21 @@ void TC5_Handler (void) {
 
 
 
-/* 
- *  TIMER SPECIFIC FUNCTIONS FOLLOW
- *  you shouldn't change these unless you know what you're doing
- */
 
-//Configures the TC to generate output events at the sample frequency.
-//Configures the TC in Frequency Generation mode, with an event output once
-//each time the audio sample frequency period expires.
- void tcConfigure(int sampleRate) {
+void tcConfigure(void) {
     
     GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TC4_TC5)) ; // select the generic clock generator used as source to the generic clock multiplexer
     while (GCLK->STATUS.bit.SYNCBUSY);
     
     tcReset(); //reset TC5
     
-    TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16; // Set Timer counter 5 Mode to 16 bits, it will become a 16bit counter ('mode1' in the datasheet)
+    TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16; // Set Timer counter 5 Mode to 16 bits
     
     TC5->COUNT16.CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ; // Set TC5 waveform generation mode to 'match frequency'
     
-    //set prescaler
-    //the clock normally counts at the GCLK_TC frequency, but we can set it to divide that frequency to slow it down
-    //you can use different prescaler divisons here like TC_CTRLA_PRESCALER_DIV1 to get a different range
-    TC5->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1024 | TC_CTRLA_ENABLE; //it will divide GCLK_TC frequency by 1024
+    TC5->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_ENABLE; // set prescaler to 1
     
-    
-    //The counter will count up to this value (it's a 16bit counter so we use uint16_t)
-    //this is how we fine-tune the frequency, make it count to a lower or higher value
-    //system clock should be 1MHz (8MHz/8) at Reset by default
-    TC5->COUNT16.CC[0].reg = (uint16_t) (SystemCoreClock / sampleRate); //set the compare-capture register. 
+    TC5->COUNT16.CC[0].reg = (uint16_t) 48000; //set the compare-capture register. 
     while (tcIsSyncing());
     
     // Configure interrupt request
@@ -98,37 +77,28 @@ void TC5_Handler (void) {
     NVIC_SetPriority(TC5_IRQn, 0);
     NVIC_EnableIRQ(TC5_IRQn);
     
-    // Enable the TC5 interrupt request
-    TC5->COUNT16.INTENSET.bit.MC0 = 1;
+    TC5->COUNT16.INTENSET.bit.MC0 = 1;// Enable the TC5 interrupt request
     while (tcIsSyncing()); //wait until TC5 is done syncing 
 } 
 
 
 
-
-
-
-//Function that is used to check if TC5 is done syncing
-//returns true when it is done syncing
-bool tcIsSyncing() {
-    return TC5->COUNT16.STATUS.reg & TC_STATUS_SYNCBUSY;
+bool tcIsSyncing() { //Function that is used to check if TC5 is done syncing
+    return TC5->COUNT16.STATUS.reg & TC_STATUS_SYNCBUSY; //returns true when it is done syncing
 }
 
-//This function enables TC5 and waits for it to be ready
-void tcStartCounter() {
+void tcStartCounter() { //This function enables TC5 and waits for it to be ready
     TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE; //set the CTRLA register
     while (tcIsSyncing()); //wait until snyc'd
 }
 
-//Reset TC5 
-void tcReset() {
+void tcReset() { //Reset TC5
     TC5->COUNT16.CTRLA.reg = TC_CTRLA_SWRST;
     while (tcIsSyncing());
     while (TC5->COUNT16.CTRLA.bit.SWRST);
 }
 
-//disable TC5
-void tcDisable() {
+void tcDisable() { //disable TC5
     TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;
     while (tcIsSyncing());
 }
