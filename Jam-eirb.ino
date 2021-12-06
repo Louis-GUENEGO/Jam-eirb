5716c71412bf4b6e2a5b3df95ae823fe77814f72
Louis GUENEGO <louis.guenego@gmail.com>
2021-12-06 13:28:56 +0100

    it now count cars

--- Jam-eirb.ino
+++ Jam-eirb.ino
@@ -1,20 +1,31 @@
+#include <stdio.h>
+#include <SPI.h>
+#include "LoRa.h"
+#include "DHT.h"
 #include "int.h"
 #include "Adafruit_APDS9960.h"
-
-// Configuration CAN
-#define PIN_LIDAR 15 // 15 => pin A0
-#define RESOLUTION 12 // ADC 12 bits => 4096 values
 
 // Configuration comunications
 #define VISTESSE_USB 115200 // vitesse de l'USB
 
 // Configuration du capteur
-#define THRESHOLD 800 // en quantum, le niveau à paritr du quel on considère les signaux
 #define MIN_TIME_MS 30 // Le temps à partir duquel on ignore le passage (elimination bruit/perturbation)
 
 //#define DEBUG_MODE // Pour voir les données brutes
 
 
+uint16_t id_node            = 236; // to change
+uint16_t id_traffic         = 558; // to change
+unsigned int freq = 865200000;//HZ
+/*network */ 
+#define WITH_APPKEY
+unsigned int nCycle = 60;// in second 300
+unsigned short id_frame = 0;
+#ifdef WITH_APPKEY
+uint8_t my_appKey[4]={5, 6, 7, 8};
+#endif
+unsigned int cpt=1;
+char message[100];
 
 
 
@@ -27,14 +38,25 @@ unsigned char voie = 3;
 
 
 
-
 Adafruit_APDS9960 apds; //create the APDS9960 object
 
 
 
 void setup() {
     Serial.begin(VISTESSE_USB);
-    analogReadResolution(RESOLUTION); // configuration du CAN
+    delay(2000);
+
+    if (!LoRa.begin(freq)) {
+      Serial.println("Starting LoRa failed!");
+      while (1); 
+    }
+  
+    LoRa.setSpreadingFactor(12);
+    LoRa.setSignalBandwidth(125E3);
+    LoRa.setCodingRate4(5);
+    LoRa.setPreambleLength(8);
+    LoRa.enableCrc();
+    LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
     
     apds.begin();
     apds.enableProximity(true); //enable proximity mode
@@ -44,16 +66,31 @@ void setup() {
     
     tcConfigure();
     tcStartCounter();
+    
+    //tcDisable();
+    nbr_auto = 0;
+    //tcDisable();
 }
 
 
 
 void loop() {
   
-  //void
+  Serial.print("Traffic : ");
+  Serial.println(nbr_auto);
+  
+  Serial.print("Frame Number : ");
+  Serial.println(cpt);
+  cpt++;
+  Serial.print("\n");
+    
+  /*Send Data via LoRa Module*/
+  send_data();
+
+  /* wait for #idlePeriodInMin Minute */
+  for (int j=0;j<nCycle;j++) {delay(1000);}
 
 }
-
 
 
 
@@ -74,9 +111,43 @@ void routine (void) { // interrupt routine (1kHz)
     } else if ( time_ms > MIN_TIME_MS ) {
         Serial.print(time_ms);
         Serial.print(" ms\n");
+        nbr_auto++;
+        Serial.print(nbr_auto);
+        Serial.print("\n");
         time_ms = 0;
     } else {
         time_ms = 0;
     }
   #endif
+}
+
+void send_data(void){
+  uint8_t app_key_offset = 0;
+  int e;
+  #ifdef WITH_APPKEY
+      app_key_offset = sizeof(my_appKey);
+      memcpy(message,my_appKey,app_key_offset); // on copie my_appKey dans le début du message
+  #endif
+      uint8_t r_size;
+      char final_str[80] = "\\";
+      char aux[10] = "";
+      char id[1] = "";
+      sprintf(final_str, "%s!%i!%hd", final_str,id_node, id_frame++);
+     
+      sprintf(aux,"%4.1f", (float)nbr_auto);
+      sprintf(final_str, "%s#%d/%s", final_str, id_traffic, aux);
+      
+
+      r_size=sprintf(message+app_key_offset,final_str);
+    
+      Serial.print(message);
+      Serial.println(r_size);
+      
+      LoRa.beginPacket();
+      LoRa.print(message);
+      LoRa.endPacket(); 
+      
+      Serial.println("\nDATA sent correctly !");
+
+      nbr_auto = 0;
 }
